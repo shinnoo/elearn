7d5a51994c7e53e770b774eb9d0ae972e4ed56ea
trandung <trandung164.ptit@gmail.com>
2020-05-20 21:15:44 +0700

    add box shadow

--- src/main/webapp/app/app-routing.module.ts
+++ src/main/webapp/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { errorRoute } from './layouts/error/error.route';
 import { navbarRoute } from './layouts/navbar/navbar.route';
-import { DEBUG_INFO_ENABLED } from 'app/app.constants';
 import { sidebarRoute } from './layouts/sidebar/sidebar.route';
 
 const LAYOUT_ROUTES = [navbarRoute, ...errorRoute, sidebarRoute];
